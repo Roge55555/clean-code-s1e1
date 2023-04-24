d9c68f351be76972d898aff8df2c918b628c57b3
Roge55555 <roge55555@gmail.com>
2023-04-25 01:52:53 +0300

    refactor: apply advanced HTML formatting according to rule 1.2(add alt-atribut)

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="button delete";
   deleteButtonImg.src="./remove.svg";
   deleteButtonImg.className = "delete-button-img";
+  deleteButtonImg.alt = "";
   deleteButton.appendChild(deleteButtonImg);
 
 
